97f439dc7e1be9b1f4695069a86fb977cca4fe12
danieldiniz1 <danieldiniz@gmail.com>
2025-05-14 19:30:53 -0300

    inital commit

---
+++ src/test/java/br/com/sh/apiexample/ApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sh.apiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
